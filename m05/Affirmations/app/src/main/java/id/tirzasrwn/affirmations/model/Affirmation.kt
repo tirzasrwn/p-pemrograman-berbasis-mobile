d89a0b9417ba528156b2666d981e41e42f3b2fca
tirzasrwn <tirzasrwn@gmail.com>
2024-10-12 10:12:58 +0700

    feat: Add a scrollable list - Affirmations.

---
+++ m05/Affirmations/app/src/main/java/id/tirzasrwn/affirmations/model/Affirmation.kt
@@ -0,0 +1,7 @@
+package id.tirzasrwn.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+/** [Affirmation] is the data class to represent the Affirmation text and imageResourceId */
+data class Affirmation(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int)
