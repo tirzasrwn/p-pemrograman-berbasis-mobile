a898a05c1afe31e66db83710a730262431181396
tirzasrwn <tirzasrwn@gmail.com>
2024-11-17 12:23:24 +0700

    feat: Add Unscramble app.

---
+++ m08/Unscramble/app/src/main/java/id/tirzasrwn/unscramble/MainActivity.kt
@@ -0,0 +1,27 @@
+package id.tirzasrwn.unscramble
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import id.tirzasrwn.unscramble.ui.GameScreen
+import id.tirzasrwn.unscramble.ui.theme.UnscrambleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            UnscrambleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    GameScreen()
+                }
+            }
+        }
+    }
+}
